baf2972934290e9e6e539212763ce090eb66c039
Ronaldo de Lélis <delelis.locutor@gmail.com>
2020-03-27 14:59:50 -0300

    "criação das validações no Front e no Back do Form Cadastro"

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\BookRequest;
 use App\Models\ModelBook;
 use App\User;
 
@@ -41,7 +41,7 @@ class BookController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BookRequest $request)
     {
         $cad = $this->objBook->create([
             'title' => $request->title,
@@ -86,7 +86,7 @@ class BookController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BookRequest $request, $id)
     {
         //
     }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,26 +2,34 @@
 
 @section('content')
 
+    @if (isset($errors) && count($errors) > 0)
+        <div class="text-center mt-4 mb-4 p-2 alert-danger">
+            @foreach ($errors->all() as $erro)
+                {{ $erro }}<br>
+            @endforeach
+        </div>
+    @endif
+
     <div class="col-md-6">
         <form name="formCad" id="formCad" method="POST" action="{{ url('/books') }}">
         {{ csrf_field() }}
        
             <strong>Título:</strong>
-            <input type="text" class="form-control" name="title">
+            <input type="text" class="form-control" name="title" required>
             <strong>Autor:</strong>
-            <select class="form-control" name="id_user" id="id_user">
+            <select class="form-control" name="id_user" id="id_user" required>
                 <option value="">Selecione</option>
                 @foreach ($users as $user)
             <option value="{{ $user->id }}">{{ $user->name }} </option>
                 @endforeach
             </select>
             <strong>Páginas:</strong>
-            <input type="text" class="form-control" name="pages">
+            <input type="text" class="form-control" name="pages" required>
             <strong>Preço:</strong>
-            <input type="text" class="form-control" name="price"> 
-        <button type="submit" class="btn btn-success" style="margin: 5px; float: left">Cadastrar</button>
-        <a href="{{ url('/books') }}"> <button class="btn btn-primary" style="margin: 5px; float: right">Voltar</button></a>
+            <input type="text" class="form-control" name="price" required> 
+        <button type="submit" class="btn btn-success" style="margin: 15px; float: left">Cadastrar</button>
         </form>
+        <a href="{{ url('/books') }}"> <button class="btn btn-primary" style="float: right">Voltar</button></a>
     </div>
 
 @endsection
